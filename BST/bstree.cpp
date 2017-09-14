80cc5ba9a8c48af8c2ea1e55a9ef61c17a9cc27f
aluno-uffs <ricardo.dparizotto>
2017-09-14 17:49:02 -0300

    delete

--- BST/bstree.cpp
+++ BST/bstree.cpp
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 struct node{
-	int key;  					//chave de identificação do nodo	
-	node * left, * right;		//ponteiros para os filhos
+	int key;  							//chave de identificação do nodo	
+	node * left, * right, * parent;		//ponteiros para os filhos
 };
 
-
-
+struct tree{
+	node * raiz;
+};
 
 //busca por uma chave k na árvore com raiz r
 //caso a chave não estiver na árvore retorna NULL
@@ -23,25 +23,31 @@ node * search (node * r, int key){
 
 
 //mostra todas as chaves da árvore
+void inorder (node * r){
+	if(r){
+		inorder(r->left);
+		printf("%d ", r->key);
+		inorder(r->right);
+	}
+}
 
-void * print (node * r);
 
 //insere um nodo com chave key na árvore com raiz r
 //retorna um ponteiro para a raiz da árvore
 
 node * insert (node * r, int key){
 	node * n = (node *)malloc(sizeof(node));		//cria um novo nodo
-	n->left = n->right = NULL;
+	n->left = n->right = n->parent = NULL;			//atualiza os valores dos ponteiros do nodo novo
 	n->key = key;
 			
-	if(!r) return n;			// se a arvore estiver vazia
+	if(!r) return n;								// se a arvore estiver vazia
 
 	node * it = r;	
 
 	while(1){
 		if(key < it->key){
 			if(!it->left){
-				it->left = n; 		//atualiza o filho da esquerda de r	
+				it->left = n; 						//atualiza o filho da esquerda de r	
 				break;		
 			}
 			it = it->left;	
@@ -53,13 +59,62 @@ node * insert (node * r, int key){
 			it = it->right;
 		}		
 	}
+	
+	n->parent = it; 										//atualiza o pai do novo nodo
 
 	return r;
 }
 
+node * tree_min(node * r){
+	return (!r->left)? r : tree_min(r->left);
+}
+
+//retorna o sucessor do nodo x
+//quem não entender olha no livro do cormen :)
+node * tree_sucessor(node * x){
+	if(x->right) return tree_min(x->right);
+
+	node * y = x->parent;
+
+	while(y && x == y->right){
+		x = y;
+		y = y->parent;
+	}
+	return y;
+}
+
+void transplant(tree * T, node * u, node * v){
+	if(!u->parent)
+		T->raiz = v;
+	else if( u == u->parent->left )
+		u->parent->left = v;
+	else u->parent->right = v;
+	
+	if(!v)
+		v->parent = u->parent;
+}
+
 //remove o nodo da árvore com raiz r que possui chave igual a key
 //(lembra que tem que dar free no nodo)
-void remove (node * r, int key);
+void tree_delete(tree * T, node * z){
+	node * y;
+
+	if(!z->left)
+		transplant(T, z, z->right);
+	else if(!z->right)
+		transplant(T, z, z->left);
+	else{
+		y = tree_min(z->right);
+		if(y->parent != z){
+			transplant(T, y, y->right);
+			y->right = z->right;
+			y->right->parent = y;
+		}
+		transplant(T, z, y);
+		y->left = z->left;
+		y->left->parent = y;
+	}	
+}
 
 
 
@@ -67,15 +122,35 @@ int main ( void ){
 
 	int n;
 	
-	node * root = NULL;
+	tree t;
+	
+
+
+	t.raiz = NULL;
+
+	node * aux;
 
 	
 	while(scanf("%d", &n)){
-		root = insert(root, n);
+		t.raiz = insert(t.raiz, n);
 	}
 
-	if(search(root, 7)) printf("achouUU!");
-	else printf("nao achooou :(");
+	if((aux = search(t.raiz, 7))) {
+		aux = tree_sucessor(aux);
+		if(aux)
+			printf("%d ", aux->key);
+	}
+
+	tree_delete(&t, aux);
+
+	inorder(t.raiz);
+
+
+
+
+
+
+	
 	
 	
 	
